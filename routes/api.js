8993047a6bfedef84aa8ac0d079a3304a71a1253
Lucas Q <lucas.14.96@gmail.com>
2021-05-13 20:14:52 -0500

    GCreated get route

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+     .then(dbWorkout => {
+         res.json(dbWorkout);
+     })
+     .catch(err => {
+         res.status(400).json(err);
+     });
+});
