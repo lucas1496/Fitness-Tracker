2410a6df4ae582ebb5545adbc9bbc1626446c70d
Lucas Q <lucas.14.96@gmail.com>
2021-05-13 22:56:33 -0500

    Finished adding api.js routes but post/put are not functional

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@ router.get("/api/workouts", (req, res) => {
      });
 });
 
-router.post("/api/workouts", ({body}, res) =>{
+router.post("/api/workouts/", ({body}, res) =>{
     Workout.create(body)
      .then(dbWorkout => {
          res.json(dbWorkout);
@@ -21,5 +21,39 @@ router.post("/api/workouts", ({body}, res) =>{
      })
 });
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    console.log(body)
+    Workout.findByIdAndUpdate(params.id,
+        { $push: { exercises: body } }, { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(10)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndRemove(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
